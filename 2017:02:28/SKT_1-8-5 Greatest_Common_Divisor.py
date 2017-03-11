370ced7ce994314e5fa6549ff65efbec15c67447
Jarry Shaw <jarryshaw@icloud.com>
2017-03-11 22:10:23 +0800

    2017/02/28作業更新
    SKT_1-8-5  Maximum Common Factor
    SKT_1-8-6  Coprimality Test
    SKT_1-8-7  Bézout Equation

--- 2017:02:28/SKT_1-8-5 Greatest_Common_Divisor.py
+++ 2017:02:28/SKT_1-8-5 Greatest_Common_Divisor.py
@@ -3,14 +3,14 @@
 #廣義歐幾里德除法
 #返回100,000內任意兩整數的最大公因數
 
-def maxCommonFactor(a=1, b=1):
+def GCD(a=1, b=1):
     if a < 0:   a = -1 * a              #將a轉為正整數進行計算
     if b < 0:   b = -1 * b              #將b轉為正整數進行計算
     if a < b:   c = a;  a = b;  b = c   #交換a與b的次序，使得a≥b
     if b == 0:  return a                #(r,0) = r
     
     r = a % b
-    return maxCommonFactor(r, b)        #(a,b) = (r_-2,r_-1) = (r_-1,r_0) = … = (r_n,r_n+1) = (r_n,0) = r_n
+    return GCD(r, b)        #(a,b) = (r_-2,r_-1) = (r_-1,r_0) = … = (r_n,r_n+1) = (r_n,0) = r_n
 
 if __name__ == '__main__':
     while True:
@@ -35,4 +35,4 @@ if __name__ == '__main__':
             continue
         break
 
-    print '(%d,%d) = %d' %(a, b, maxCommonFactor(a, b))
+    print '(%d,%d) = %d' %(a, b, GCD(a, b))
