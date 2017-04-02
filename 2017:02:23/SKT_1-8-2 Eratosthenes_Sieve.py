be3d97b2b00a63072d60e73339a90fa2f9e7b91c
Jarry Shaw <jarryshaw@icloud.com>
2017-04-02 11:16:18 +0800

    2017/02/23作業更新
    Minor bugs fixed.

--- 2017:02:23/SKT_1-8-2 Eratosthenes_Sieve.py
+++ 2017:02:23/SKT_1-8-2 Eratosthenes_Sieve.py
@@ -8,10 +8,11 @@ import math
 def eratosthenesSieve(N=10000):
     set = [1]*(N+1)                 #用於存儲N(默认为10000）個正整數的表格／狀態；其中，0表示篩去，1表示保留
     for index in range(2,int(math.sqrt(N))):  #篩法（平凡除法）
-        ctr = 2
-        while index * ctr <= N:
-            set[index*ctr] = 0      #將index的倍數篩去
-            ctr += 1
+        if set[index] == 1:
+            ctr = 2
+            while index * ctr <= N:
+                set[index*ctr] = 0      #將index的倍數篩去
+                ctr += 1
 
     rst = []
     for ptr in range(2,N):          #獲取結果
--- 2017:02:23/SKT_1-8-4 Trivial_Division.py
+++ 2017:02:23/SKT_1-8-4 Trivial_Division.py
@@ -9,19 +9,19 @@ def trivialDivision(N=2):
     if N < 0:   N = -1 * N
     if N == 1 or N == 0:    raise ValueError
     
-    set = eratosthenesSieve(N)    #得出小於N的所有素數
-    for num in set:                 #素性判斷
-        if N % num == 0:
-            return 0                #N為合數
-    return 1                        #N為素數
+    set = eratosthenesSieve(N+1)      #得出小於N的所有素數
+    if N in set:                    #素性判斷
+        return 1                    #N為素數
+    return 0                        #N為合數
 
 def eratosthenesSieve(N):
     set = [1]*(N+1)                 #用於存儲N個正整數的表格／狀態；其中，0表示篩去，1表示保留
     for index in range(2,int(math.sqrt(N))):  #篩法（平凡除法）
-        ctr = 2
-        while index * ctr <= N:
-            set[index*ctr] = 0      #將index的倍數篩去
-            ctr += 1
+        if set[index] == 1:
+            ctr = 2
+            while index * ctr <= N:
+                set[index*ctr] = 0      #將index的倍數篩去
+                ctr += 1
 
     rst = []
     for ptr in range(2,N):          #獲取結果
@@ -42,7 +42,7 @@ if __name__ == '__main__':
             print 'Invalid input.'
             continue
         break
-    
+
     if trivialDivision(N):
         print 'N is a prime number.'
     else:
